1b00178f831270201b063e5453af86a10e1848dd
zoujin <zoujin@zj.com>
2025-02-23 20:51:36 +0800

    修复不能使用小数点

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,10 @@
 
 <script setup lang="ts">
-import MyCounter from './components/MyCounter.vue';
+import MyCounterType from './components/MyCounterType.vue';
 </script>
 
 <template>
-  <MyCounter title="vue3 + vue ant design 开发 计算器" author="zj"/>
+  <MyCounterType title="vue3 + vue ant design 开发 计算器" author="zj"/>
 </template>
 
 <style>
--- src/components/MyCounterType.vue
+++ src/components/MyCounterType.vue
@@ -38,9 +38,10 @@ const inputValue =ref('') //显示操作符
 const numberOne=ref<number|null>(null)
 const opt=ref('')
 const numberTow=ref<number|null>(null)
-const isCalculated=ref(false) //标记是否完成第一次运算
+const isCalculated=ref(true) //标记是否完成第一次运算
 const inputValueTemp=ref('')//记录第二个数值
 //ts计算
+//当前bug输入0数值使用运算符数值等于就会报错
 const calc=()=>{
   numberOne.value=eval(inputValue.value)
   inputValue.value=String(numberOne.value)
@@ -90,11 +91,11 @@ const poInt =(decimal:string)=>{
             <Button type="primary" shape="round" @click="numberButton('1')" v-text="1" ></Button>
             <Button type="primary" shape="round" @click="numberButton('2')" v-text="2" ></Button>
             <Button type="primary" shape="round" @click="numberButton('3')" v-text="3" ></Button>
-            <Button type="primary" shape="round" class="calc-opt" @click="numberButton('*')">-</Button>
+            <Button type="primary" shape="round" class="calc-opt" @click="numberButton('-')">-</Button>
             <br />
             <Button type="primary" shape="round" @click="clearDisplay">C</Button>
             <Button type="primary" shape="round" @click="numberButton('0')" v-text="0" ></Button>
-            <Button type="primary" shape="round" @click="poInt('.')">.</Button>
+            <Button type="primary" shape="round" @click="numberButton('.')">.</Button>
             <Button type="primary" shape="round" class="calc-opt" @click="numberButton('+')">+</Button>
             <div style="width: 160px;margin-top: 3px;">
               <Button type="primary" shape="round" style="background-color: black;"  @click="calc()" :block="true">=</Button>
