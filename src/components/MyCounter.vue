a52422f5785b3fc4fa3720606df316c6ec030556
zoujin <zoujin@zj.com>
2025-02-23 13:25:38 +0800

    完成计算功能，修覅已知bug

--- src/components/MyCounter.vue
+++ src/components/MyCounter.vue
@@ -34,33 +34,60 @@ defineProps<Props>();
 const activeKey = ref(['1']);
 
 //创建响应式数据
-let inputValue =ref('')
+const inputValue =ref('')
+const numberOne=ref<number|null>(null)
+const opt=ref('')
+const numberTow=ref<number|null>(null)
 
-
-//post方法
+//post方法，计算
 const calc = async () => {
+  numberTow.value=parseFloat(inputValue.value)
   try {
     const response = await axios.post("/calculator/calc", {
-      num1: inputValue.value,
-      num2: 0,
-      opt: 1
+      num1:numberOne.value,
+      num2: numberTow.value,
+      opt: opt.value
     });
-  } catch (error) {
+    //重置状态
+    numberTow.value=0;
+    opt.value='0';
+    inputValue.value=response.data;//将返回的结果显示给input
+  } catch (error){
     console.error('请求出错:', error);
   }
 };
-//接收计算器按钮的值，显示到Input
-const numberButton = (a:string)=>{
-  inputValue.value+=a
-  console.log(inputValue.value)
+
+
+//接收计算器按钮的值，显示到Input,如果值是0直接输入值，如果不是0就拼接前面的值
+const numberButton = (number:number)=>{
+  if(inputValue.value==='0'){
+    inputValue.value=number.toString()
+  }else{
+    inputValue.value+=number.toString()
+  }
+  // inputValue.value+=a
+  // console.log(inputValue.value)
 }
 //清空input
 const clearDisplay=()=>{ 
   inputValue.value=''
+  
 }
-//按下加法按钮时，继续显示到文本框
-const jia = (b:string)=>{
-  inputValue.value+=b
+//按下计算按钮,判断一个值是否为空，如果不是空，就把第一个值给firstNumber，如果是空就直接计算
+const  jisuan = (b:string)=>{
+  if (numberOne.value === null) {
+      numberOne.value = parseFloat(inputValue.value);//string转换float
+    } else {
+      calc();
+    }
+    opt.value = b;
+    inputValue.value = '0';//赋值第一个数完成后，清空inputValue
+}
+//添加小数点
+const poInt =(decimal:string)=>{
+  if(!inputValue.value.includes(decimal)){
+    inputValue.value+=decimal;
+  }
 }
 </script>
 
@@ -77,25 +104,25 @@ const jia = (b:string)=>{
           </div>
           <div>
             <Button type="primary" shape="round" @click="clearDisplay">C</Button>
-            <Button type="primary" shape="round" @click="numberButton('7')">7</Button>
-            <Button type="primary" shape="round" @click="numberButton('8')" v-text="8" ></Button>
-            <Button type="primary" shape="round" @click="numberButton('9')" v-text="9" ></Button>
+            <Button type="primary" shape="round" @click="numberButton(7)">7</Button>
+            <Button type="primary" shape="round" @click="numberButton(8)" v-text="8" ></Button>
+            <Button type="primary" shape="round" @click="numberButton(9)" v-text="9" ></Button>
             <Button type="primary" shape="round" class="calc-opt">/</Button>
             <br />
-            <Button type="primary" shape="round" @click="numberButton('4')" v-text="4" ></Button>
-            <Button type="primary" shape="round" @click="numberButton('5')" v-text="5" ></Button>
-            <Button type="primary" shape="round" @click="numberButton('6')" v-text="6" ></Button>
+            <Button type="primary" shape="round" @click="numberButton(4)" v-text="4" ></Button>
+            <Button type="primary" shape="round" @click="numberButton(5)" v-text="5" ></Button>
+            <Button type="primary" shape="round" @click="numberButton(6)" v-text="6" ></Button>
             <Button type="primary" shape="round" class="calc-opt">*</Button>
             <br />
-            <Button type="primary" shape="round" @click="numberButton('1')" v-text="1" ></Button>
-            <Button type="primary" shape="round" @click="numberButton('2')" v-text="2" ></Button>
-            <Button type="primary" shape="round" @click="numberButton('3')" v-text="3" ></Button>
-            <Button type="primary" shape="round" class="calc-opt">-</Button>
+            <Button type="primary" shape="round" @click="numberButton(1)" v-text="1" ></Button>
+            <Button type="primary" shape="round" @click="numberButton(2)" v-text="2" ></Button>
+            <Button type="primary" shape="round" @click="numberButton(3)" v-text="3" ></Button>
+            <Button type="primary" shape="round" class="calc-opt" @click="jisuan('2')">-</Button>
             <br />
-            <Button type="primary" shape="round" @click="numberButton('0')" v-text="0" ></Button>
-            <Button type="primary" shape="round">.</Button>
+            <Button type="primary" shape="round" @click="numberButton(0)" v-text="0" ></Button>
+            <Button type="primary" shape="round" @click="poInt('.')">.</Button>
             <Button type="primary" shape="round" style="background-color: black;"  @click="calc()">=</Button>
-            <Button type="primary" shape="round" class="calc-opt" @click="jia('+')">+</Button>
+            <Button type="primary" shape="round" class="calc-opt" @click="jisuan('1')">+</Button>
           </div>
         </div>
 
